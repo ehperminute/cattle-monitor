01334cc49a3cfecf4ba7fe64b24a5ac930a34551
ehperminute <87844955+ehperminute@users.noreply.github.com>
2026-04-12 19:19:43 -0600

    Update train.py

--- ml/train.py
+++ ml/train.py
@@ -1,17 +1,15 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+from config import FEATURES, TARGET
+
 
 df = pd.read_csv("final_data.csv")  
 
-features = [
-    "Age","Weight","Body_Temperature","Heart_Rate",
-    "Appetite_Loss","Vomiting","Diarrhea","Coughing"
-]
-
-X = df[features]
-y = df["Disease"]
+X = df[FEATURES]
+y = df[TARGET]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
