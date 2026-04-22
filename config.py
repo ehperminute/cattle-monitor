500bdf86324f5282d0694e8e2a7135c1af1966ef
ehperminute <87844955+ehperminute@users.noreply.github.com>
2026-04-22 13:46:17 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 FEATURES = [
-    "Age","Weight","Body_Temperature","Heart_Rate",
-    "Appetite_Loss","Vomiting","Diarrhea","Coughing"
+    "Age",
+    "Weight",
+    "Body_Temperature",
+    "Heart_Rate",
+    "Appetite_Loss",
+    "Vomiting",
+    "Diarrhea",
+    "Coughing",
 ]
 
 TARGET = "Disease"
+
+DATA_PATH = "data/final_data.csv"
+MODEL_PATH = "model/disease_model.joblib"
